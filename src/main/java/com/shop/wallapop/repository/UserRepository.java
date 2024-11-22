665fda92311690cebfb33cf6819e383b209a6e86
MAL501 <marcosaragonlopez@gmail.com>
2024-11-22 18:05:40 +0100

    Crear login

--- src/main/java/com/shop/wallapop/repository/UserRepository.java
+++ src/main/java/com/shop/wallapop/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.shop.wallapop.repository;
 
-import com.shop.wallapop.DTO.UserDTO;
 import com.shop.wallapop.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- src/main/java/com/shop/wallapop/service/CustomUserServiceDetailsService.java
+++ src/main/java/com/shop/wallapop/service/CustomUserServiceDetailsService.java
@@ -1,6 +1,5 @@
 package com.shop.wallapop.service;
 
-import com.shop.wallapop.DTO.UserDTO;
 import com.shop.wallapop.entity.Usuario;
 import com.shop.wallapop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
